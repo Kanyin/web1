a75d055af09727e5eb2a6bf8b542af4845dc1506
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-31 16:19:08 -0500

    Update App.tsx to incorporate ScrollToTop.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
+import ScrollToTop from "./components/layout/ScrollToTop";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -20,6 +21,7 @@ const App = () => (
       <Sonner />
 
       <HashRouter>
+       <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
