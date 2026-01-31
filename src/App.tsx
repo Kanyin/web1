980543bbb3a8abdb961ab80c45ad3a36249be010
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-31 15:32:17 -0500

    Update App.tsx to remove clients based on Loveable

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Clients from "./pages/Clients";
+/*import Clients from "./pages/Clients";*/
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
