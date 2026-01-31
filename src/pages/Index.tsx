5ea124642ec60df95af330f197a9f1e555a7be5f
Kanyin Olagbegi <kanyin.olagbegi@gmail.com>
2026-01-31 15:36:10 -0500

    Update Index.tsx to hide Testimonials Section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/layout/Layout";
 import Hero from "@/components/home/Hero";
 import Introduction from "@/components/home/Introduction";
 import ServiceHighlights from "@/components/home/ServiceHighlights";
-import Testimonials from "@/components/home/Testimonials";
+/*import Testimonials from "@/components/home/Testimonials";*/
 import CallToAction from "@/components/home/CallToAction";
 
 const Index = () => {
